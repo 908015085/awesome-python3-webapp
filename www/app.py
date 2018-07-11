f2983942871057363ce3b7320f9b17efbf5edbdb
yangfang <908015085@qq.com>
2018-07-11 09:59:02 +0800

    编写配置文件

---
+++ www/app.py
@@ -0,0 +1,40 @@
+
+import time
+from datetime import datetime
+
+
+async def logger_factory(app, handler):
+	async def logger(request):
+		logging.info('Request: %s %s' % (request.method, request.path))
+		return (await handler(request))
+	return logger
+
+	
+async def data_factory(app, handler):
+	async def parse_data(request):
+		if request.method == 'POST':
+			if request.content_type.startswith('application/json'):
+				request.__data__ = await request.json()
+				logging.info('request json: %s' % str(request.__data__))
+			elif request.content_type.startswith('application/x-www-form-urlencoded'):
+				request.__data__ = await request.post()
+				logging.info('request form: %s' % str(request.__data__))
+		return  (await handler(request))
+	return parse_data
+
+
+def datetime_filter(t):
+	delta = int(time.time() - t)
+	if delta < 60:
+		return u'1分钟前'
+	if delta < 3600:
+		return u'%s分钟前' % (delta // 60)
+	if delta < 86400:
+		return u'%s分钟前' % (delta // 3600)
+	if delta < 604800:
+		return u'%s分钟前' % (delta // 86400)
+	dt = datetime.fromtimestamp(t)
+	return u'%s年%s月%s日' % (dt.year, dt.month, dt.day)
+
+
+	
